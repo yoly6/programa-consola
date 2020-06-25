d2d52c53f7b402256311f3fa73f1b9d036e3a945
Yolanda Marlene Perez Paccha <yolanda.perez@invitado.epn.edu.ec>
2020-06-24 20:27:03 -0500

    primer deber de consola

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] argumentos)
+
+        {
+            if (argumentos.Length > 0)
+            
+
+                Console.Write(argumentos[0]);
+                {
+                    Console.Write("Mi primera consola \n ");
+                    Console.WriteLine("Introduce tus nombres...");
+                    string datosIngresados = Console.ReadLine();
+                    Console.WriteLine("Introduce tus apellidos...");
+                    int datosIngresados5 = Console.Read();
+                    Console.WriteLine("datos ingresados" + datosIngresados);
+                    string datosIngresados2 = Console.ReadLine();
+                }
+            
+        }
+    }
+ }
+
